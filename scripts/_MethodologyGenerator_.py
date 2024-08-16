38035fb1efb3a632a66158ee79ded81faf607fb9
GuilhermeCgSzk <guilhermecsz11@gmail.com>
2024-08-16 02:45:03 -0300

    mais alterações

--- scripts/_MethodologyGenerator_.py
+++ scripts/_MethodologyGenerator_.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import os
+import sklearn
+import pyts
 import math
 import pyts.image as proj
 import numpy
@@ -32,6 +34,74 @@ def save(projected_signal, projection_name, color, path):
 		cmap=color
 	)
 	plt.close()
+
+class GramianAngularFieldFigures:
+	def polar_coordinates(self, signal, path):
+		signal = numpy_rescale(signal,dst_min_val=-1,dst_max_val=1, axis=0)
+		
+		fig, ax = plt.subplots(figsize=(10,7), subplot_kw={'projection':'polar'})
+		
+		ax.plot([math.acos(x) for x in signal],list(range(len(signal))), color='red')
+		
+		ax.set_theta_zero_location("W")
+		ax.set_theta_direction('clockwise') 
+		ax.set_thetamin(0)
+		ax.set_thetamax(180)
+		fig.savefig(os.path.join(path,'polar'), dpi=300)
+		plt.close()
+
+	def projection_figure(self, signal, path, *, method):
+		reshaped_signal = signal.reshape(tuple(reversed(signal.shape)))
+		matrix = proj.GramianAngularField(method=method).transform([reshaped_signal])[0]
+		save(matrix, f'GramianAngularField({method})', 'Reds', path)
+
+class MarkovTransitionFieldFigures:
+	def __init__(self, n_bins):
+		self.n_bins = n_bins
+		
+	def quantile_bins_figure(self, signal, path):
+		sklearn_signal = sklearn.preprocessing.RobustScaler().fit_transform([[sample] for sample in signal])
+		sklearn_signal = sklearn_signal - (sklearn_signal.max()+sklearn_signal.min())/2
+		signal =[sample[0] for sample in sklearn_signal]	
+		
+		fig, ax = plt.subplots(figsize=(10, 5))
+		
+		def subsample2(arr, percentage):
+			return [arr[int(i)] for i in np.arange(0,len(arr),len(arr)/percentage)]
+		colors = subsample2(['darkred','red','darkorange','gold','darkkhaki','olive','limegreen','darkgreen'],self.n_bins)
+		[bins] = pyts.preprocessing.KBinsDiscretizer(n_bins=self.n_bins).transform([signal])
+		
+		qbin = -1
+		for x in range(len(signal)):
+			if bins[x] != qbin:
+				qbin = bins[x]
+				ax.plot(np.arange(x,len(signal)),signal[x:], color=colors[bins[x]], linewidth=3)
+				
+		for bin_i in range(1,self.n_bins):
+			minimum = 1
+			for x,b in enumerate(bins):
+				if b == bin_i and signal[x]<minimum:
+					minimum = signal[x]
+			ax.plot([0,len(signal)],[minimum,minimum],color='black',linestyle='dotted')
+					
+				
+				
+		ax.set_xlim(0,len(signal))
+		ax.set_xticks([])
+		ax.set_yticks([])
+			
+		fig.savefig(os.path.join(path,'QuantileBins'), dpi=300)
+		plt.close()
+		
+	def reconstruction(self, signal, path):
+		[bins] = pyts.preprocessing.KBinsDiscretizer(n_bins=self.n_bins).transform([signal])
+		
+
+	def projection_figure(self, signal, path):
+		reshaped_signal = signal.reshape(tuple(reversed(signal.shape)))
+		matrix = proj.MarkovTransitionField(n_bins=self.n_bins).transform([reshaped_signal])[0]
+		save(matrix, f'MarkovTransitionField', 'Greens', path)
+	
 
 class RecurrencePlotFigures:
 	def __init__(self, delay, threshold):
@@ -117,3 +187,12 @@ class MethodologyGenerator(Generator):
 		rp_figures.time_delay_phase_space_figure(signal, path, show_recurrences=True)
 		rp_figures.projection_figure(signal, path)
 		rp_figures.projection_figure(signal, path, thresholded=False)
+		
+		gaf_figures = GramianAngularFieldFigures()
+		gaf_figures.polar_coordinates(signal, path)
+		gaf_figures.projection_figure(signal, path, method='summation')
+		gaf_figures.projection_figure(signal, path, method='difference')
+		
+		mtf_figures = MarkovTransitionFieldFigures(8)
+		mtf_figures.quantile_bins_figure(signal, path)
+		mtf_figures.projection_figure(signal, path)
