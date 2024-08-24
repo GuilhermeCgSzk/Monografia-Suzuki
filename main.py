beb461baf5cfc5baca65d2ec321d290b81ef9c92
GuilhermeCgSzk <guilhermecsz11@gmail.com>
2024-08-23 22:57:08 -0300

    ajuste na main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 from scripts._Names_ import Pair_Group, Pair
 from scripts._Names_ import *
@@ -7,8 +8,6 @@ from scripts._Model_Names_  import *
 
 if __name__=='__main__':
 	from scripts import *
-	
-	MethodologyGenerator().generate('img/methodology')
 	
 	df = pd.read_csv('data/results67+70.csv')
 	df = Preprocessor(df).get()
@@ -33,17 +32,27 @@ if __name__=='__main__':
 	
 	#ViolinplotGenerator(df).generate('img/resultados/violinplots')
 	
+	averages_table_dir = 'tex/tabelas/resultados/averages/models'
+	os.makedirs(averages_table_dir, exist_ok=True)
+	
 	averages_table_generator = AveragesTableGenerator(df)
-	averages_table_generator.generate('tex/tabelas/resultados/averages/models')
-	averages_table_generator.generate_for_name_obj(Aeon_Group,'tex/tabelas/resultados/averages/models',delete_repeated=False)
-	averages_table_generator.generate_per_pair_group('tex/tabelas/resultados/averages/models',pair_group)
+	averages_table_generator.generate(averages_table_dir)
+	averages_table_generator.generate_for_name_obj(Aeon_Group,averages_table_dir, delete_repeated=False)
+	averages_table_generator.generate_per_pair_group(averages_table_dir, pair_group)
+	
+	time_box_plot_dir = 'img/resultados/boxplots'
 	
 	time_box_plot_generator = TimeBoxplotsGenerator(benchmark_df)
-	time_box_plot_generator.generate('img/resultados/boxplots',lim=100)
-	time_box_plot_generator.generate_for_name_obj(Aeon_Group,'img/resultados/boxplots',lim=11)
-	time_box_plot_generator.generate_per_pair_group('img/resultados/boxplots',pair_group)
+	time_box_plot_generator.generate(time_box_plot_dir, lim=100)
+	time_box_plot_generator.generate_for_name_obj(Aeon_Group,time_box_plot_dir, lim=11)
+	time_box_plot_generator.generate_per_pair_group(time_box_plot_dir, pair_group)
 	
+	
+	memory_table_dir = 'tex/tabelas/resultados/memory'
 	memory_table_generator = MemoryTableGenerator(benchmark_df)
-	memory_table_generator.generate('tex/tabelas/resultados/memory')
-	memory_table_generator.generate_per_name_obj(Aeon_Group,'tex/tabelas/resultados/memory')
-	memory_table_generator.generate_per_pair_group('tex/tabelas/resultados/memory', pair_group)
+	memory_table_generator.generate(memory_table_dir)
+	memory_table_generator.generate_per_name_obj(Aeon_Group, memory_table_dir)
+	memory_table_generator.generate_per_pair_group(memory_table_dir, pair_group)
+	
+	methodology_dir = 'img/methodology'
+	MethodologyGenerator().generate(methodology_dir)
